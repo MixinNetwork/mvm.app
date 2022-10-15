85c2365159813f414f79ac072b265df7ad802bbd
hundredark <hundredark@gmail.com>
2022-10-15 11:00:09 +0800

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`
+  ],
+  theme: {
+    screens: {
+      sm: '375px',
+      md: '720px',
+      lg: '1024px',
+      xl: '1280px'
+    },
+    extend: {
+      colors: {
+        primary: '#5959D8',
+      }
+    }
+  }
+}

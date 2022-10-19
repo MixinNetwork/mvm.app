6390b7a4bd74537e31b520c84491c98cd21c822e
hundredark <hundredark@gmail.com>
2022-10-19 15:13:54 +0800

    Fix meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,22 @@
 export default {
+  ssr: false,
+  target: 'static', // default is 'server'
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mvm.app',
+    title: 'MVM - Cross-Chain Smart Contract Platform',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Cross-Chain Smart Contract Platform' },
+      { hid: 'keywords', name: 'keywords', content: 'smart contract, cross chain, mvm, evm, bridge, mixin' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
     ]
   },
 

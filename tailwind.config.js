8fab90fd2e5c13afc115737cf395f991f73774cc
hundredark <hundredark@gmail.com>
2022-10-20 17:54:36 +0800

    Add button hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#5959D8",
+        hover: "#DADDFF"
       },
       boxShadow: {
         mvm: "0px 8px 40px rgba(18, 43, 91, 0.08)",

9245777b02e246c467790ef64079533aa31afb85
hundredark <hundredark@gmail.com>
2022-10-24 15:58:59 +0800

    Update style on mobile

--- components/Home.vue
+++ components/Home.vue
@@ -34,48 +34,50 @@
     </div>
 
     <div
-      class="main relative mx-auto px-6 w-full sm:p-0 sm:w-[632px] md:w-[852px] lg:w-[1200px]"
+      class="main relative mx-auto pb-2 px-6 w-full sm:p-0 sm:w-[632px] md:w-[852px] lg:w-[1200px]"
     >
       <div
-        class="flex justify-center h-[234px] sm:absolute sm:-top-14 sm:-right-20 sm:h-[300px] md:h-[380px] md:-top-14 lg:h-[530px] lg:-top-[95px]"
+        class="flex justify-center w-full sm:w-auto sm:absolute sm:-top-14 sm:-right-20 sm:h-[300px] md:h-[380px] md:-top-14 lg:h-[530px] lg:-top-[95px]"
       >
-        <img class="h-full" :src="background" alt="background-image" />
+        <img class="w-full sm:w-auto sm:h-full" :src="background" alt="background-image" />
       </div>
 
-      <!-- title -->
-      <div
-        class="relative w-full font-bold text-[30px] sm:text-[32px] md:text-[40px] lg:text-[56px] leading-[130%]"
-      >
-        <div class="text-primary">Cross-Chain</div>
-        <div>Smart Contracts Platform</div>
-      </div>
-      <div
-        class="relative mt-2.5 text-sm opacity-60 sm:mt-3 sm:w-[400px] md:text-base md:mt-6 md:w-[500px] lg:mt-6 lg:text-xl lg:w-[700px]"
-      >
-        Open source, secure, low-cost, lightning fast and decentralized.
-      </div>
-
-      <!-- links -->
-      <div
-        class="button-container flex justify-between relative my-6 sm:w-[344px] md:w-[380px] lg:w-[440px]"
-      >
-        <div class="btn">
-          <a class="primary-btn" href="https://mvm.dev">Developer docs</a>
+      <div>
+        <!-- title -->
+        <div
+          class="relative w-full font-bold text-[30px] sm:text-[32px] md:text-[40px] lg:text-[56px] leading-[130%]"
+        >
+          <div class="text-primary">Cross-Chain</div>
+          <div>Smart Contracts Platform</div>
         </div>
-        <div class="btn">
-          <a class="default-btn" href="https://bridge.mvm.app">Bridge to MVM</a>
+        <div
+          class="relative mt-2.5 text-sm opacity-60 sm:mt-3 sm:w-[400px] md:text-base md:mt-6 md:w-[500px] lg:mt-6 lg:text-xl lg:w-[700px]"
+        >
+          Open source, secure, low-cost, lightning fast and decentralized.
         </div>
-      </div>
 
-      <!-- status -->
-      <div class="flex flex-wrap relative justify-between w-full">
-        <template v-for="(feature, i) of features">
-          <feature-box
-            :key="i"
-            :title="feature.title"
-            :content="feature.content"
-          />
-        </template>
+        <!-- links -->
+        <div
+          class="button-container flex justify-between relative my-6 sm:w-[344px] md:w-[380px] lg:w-[440px]"
+        >
+          <div class="btn">
+            <a class="primary-btn" href="https://mvm.dev">Developer docs</a>
+          </div>
+          <div class="btn">
+            <a class="default-btn" href="https://bridge.mvm.app">Bridge to MVM</a>
+          </div>
+        </div>
+
+        <!-- status -->
+        <div class="flex flex-wrap justify-between relative w-full">
+          <template v-for="(feature, i) of features">
+            <feature-box
+              :key="i"
+              :title="feature.title"
+              :content="feature.content"
+            />
+          </template>
+        </div>
       </div>
     </div>
   </div>

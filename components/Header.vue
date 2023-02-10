554e2b9b2da40ea66031cc64746ba459897b1510
hundredark <hundredark@gmail.com>
2023-02-10 12:10:42 +0800

    Fix header style

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex justify-center items-center absolute top-0 w-full my-6 px-6 z-10 sm:h-[100px] sm:w-full sm:px-0">
+  <div class="flex justify-center items-center absolute top-0 w-full my-6 px-6 z-10 sm:my-0 sm:h-[100px] sm:w-full sm:px-0">
     <div 
-      class="relative flex flex-row justify-between w-full h-6 sm:h-auto sm:w-[632px] md:w-[852px] lg:w-[1200px]"
+      class="relative flex flex-row justify-between items-center w-full h-6 sm:h-auto sm:w-[632px] md:w-[852px] lg:w-[1200px]"
     >
       <div class="flex row items-center h-6 sm:h-8">
         <img class="h-full" :src="logo" alt="mvm-logo" />
-        <div class="ml-2 font-extrabold text-xs opacity-80 sm:text-xl">MVM</div>
+        <div class="ml-2 font-extrabold text-xs leading-[14px] text-black/80 sm:text-xl sm:leading-6">MVM</div>
       </div>
 
       <div class="relative">
@@ -18,18 +18,19 @@
 
         <div 
           :class="[
-            'absolute top-7 -right-2.5 flex flex-col items-center bg-white shadow-[0_2px_20px_rgba(0,0,74,0.1)] z-10 sm:visible sm:flex sm:flex-row sm:justify-between sm:relative sm:top-0 sm:right-0 sm:m-0 sm:w-[350px] sm:border-opacity-0 sm:shadow-none', 
+            'z-10 absolute top-9 -right-3 flex flex-col items-center w-[150px] bg-white rounded-md shadow-[0_2px_20px_rgba(0,0,74,0.1)] overflow-hidden sm:visible sm:flex sm:flex-row sm:justify-between sm:static sm:w-auto sm:border-opacity-0 sm:shadow-none', 
             showNav ? 'visible' : 'invisible'
           ]"
         >
-          <div class="flex items-center w-36 h-12 opacity-80 bg-white text-sm sm:justify-center sm:m-0 sm:w-[100px] sm:text-lg">
-            <a class="block ml-6 text-black hover:font-bold sm:m-0" href="https://scan.mvm.dev">{{ $t('nav.explorer') }}</a>
-          </div>
-          <div class="flex items-center w-36 h-12 opacity-80 bg-white text-sm border-y border-[#F5F5F5] sm:justify-center sm:m-0 sm:w-[100px] sm:text-lg sm:border-opacity-0">
-            <a class="block ml-6 text-black hover:font-bold sm:m-0" href="https://bridge.mvm.app">{{ $t('nav.bridge') }}</a>
-          </div>
-          <div class="flex items-center w-36 h-12 opacity-80 bg-white text-sm sm:justify-center sm:m-0 sm:w-[100px] sm:text-lg">
-            <a class="block ml-6 text-black hover:font-bold sm:m-0" href="https://mvm.dev">{{ $t('nav.developers') }}</a>
+          <div 
+            v-for="(l, i) in nav" 
+            :key="i"
+            :class="[
+              'flex items-center w-full h-12 bg-white sm:justify-center sm:ml-10 sm:w-auto sm:h-auto',
+              i === 1 ? 'border-y border-[#F5F5F5] sm:border-opacity-0' : '',
+            ]"
+          >
+            <a class="block pl-6 font-normal text-sm leading-4 text-black/80 sm:pl-0 sm:text-lg sm:leading-[27px] hover:font-bold" :href="l.href">{{ l.text }}</a>
           </div>
         </div>
       </div>
@@ -48,6 +49,20 @@ export default {
       logo,
       menuIcon,
       showNav: false,
+      nav: [
+        {
+          href: "https://scan.mvm.dev",
+          text: this.$t('nav.explorer')
+        },
+        {
+          href: "https://bridge.mvm.app",
+          text: this.$t('nav.bridge')
+        },
+        {
+          href: "https://mvm.dev",
+          text: this.$t('nav.developers')
+        },
+      ]
     };
   },
   methods: {

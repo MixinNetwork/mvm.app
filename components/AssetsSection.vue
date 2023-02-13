ef5467e6a1d9d4fd69a64b14dab9a90e57b956d8
hundredark <hundredark@gmail.com>
2023-02-13 15:49:02 +0800

    Add scroll animation

--- components/AssetsSection.vue
+++ components/AssetsSection.vue
@@ -1,26 +1,30 @@
 <template>
-  <div class="pt-[100px] w-full sm:pt-[140px]">
-    <div class="mb-[60px] px-6 w-full font-bold text-[32px] leading-[130%] sm:mb-[160px] sm:p-0 sm:text-[50px] sm:leading-[72px] sm:text-center">
+  <div class="flex flex-col items-center pt-[100px] w-full sm:pt-[140px]">
+    <div
+      class="mb-[60px] px-6 w-full font-bold text-[32px] leading-[130%] sm:mb-[160px] sm:p-0 sm:text-[50px] sm:leading-[72px] sm:text-center">
       <span class="text-primary">{{ $t("assets.title1") }}</span>{{ $t("assets.title2") }}
     </div>
     
-    <div class="flex flex-col items-center w-full h-[156px] overflow-x-hidden sm:h-[384px]">
-      <div 
-        v-for="(row, i) in iconList" 
-        :key="i"
-        :class="[
-          'relative mb-4 w-[675px] whitespace-nowrap sm:mb-10 sm:w-[1320px] lg:w-[1675px]',
-          offset[i]
-        ]" 
-      >
-        <img 
-          class="inline-block mr-3 w-9 sm:mr-[28px] sm:w-[66px] lg:mr-[34px] lg:w-[88px]" 
-          v-for="icon in row" 
-          :key="icon.alt"
-          :src="icon.src" 
-          :alt="icon.alt" 
-        />
-      </div>
+    <div class="flex flex-col w-full h-[156px] overflow-x-hidden sm:h-[384px]">
+      <no-ssr placeholder="Loading...">
+        <div 
+          v-for="(row, i) in iconList" 
+          :key="i"
+          :class="[
+            'relative mb-4 whitespace-nowrap sm:mb-10',
+            offsets[i],
+            `animation-container${i + 1}`
+          ]" 
+        >
+          <img 
+            class="inline-block mr-3 w-9 sm:mr-9 sm:w-[88px]" 
+            v-for="(icon, i) in row" 
+            :key="i"
+            :src="icon.src" 
+            :alt="icon.alt" 
+          />
+        </div>
+      </no-ssr>
     </div>
   </div>
 </template>
@@ -43,7 +47,7 @@ import avax from "@/assets/icons/avax.webp";
 import tribe from "@/assets/icons/tribe.webp";
 import dot from "@/assets/icons/dot.webp";
 import ckb from "@/assets/icons/ckb.webp";
-import zec from "@/assets/icons/zec.webp"; 
+import zec from "@/assets/icons/zec.webp";
 import ksm from "@/assets/icons/ksm.webp";
 import dash from "@/assets/icons/dash.webp";
 import algo from "@/assets/icons/algo.webp";
@@ -69,67 +73,172 @@ import doge from "@/assets/icons/doge.webp";
 import etc from "@/assets/icons/etc.webp";
 import bsc from "@/assets/icons/bsc.webp";
 
+if (process.client) {
+  var innerWidth = window.innerWidth;
+}
+
 export default {
   name: "AssetsSection",
   data() {
     return {
-      offset: {
-        0: "-left-2.5 sm:-left-7",
-        1: "left-2.5 sm:left-7",  
-        2: ""
-      },
-      imgStyle: "mr-3 w-9 md:mr-[20px] md:w-[44px] md:mr-[28px] md:w-[66px] lg:mr-[34px] lg:w-[88px]",
+      rowWidth: innerWidth < 720 ? 672 : 1736,
+      leftOffsets: [27, 36, 12],
+      offsets: [
+        `-left-[9px] sm:-left-[27px]`,
+        `left-[12px] sm:left-[36px]`,
+        `left-[4px] sm:left-[12px]`,
+      ],
       iconList: [
         [
-          {src: stc, alt: "stc"},
-          {src: btc, alt: "btc"},
-          {src: rvn, alt: "rvn"},
-          {src: btcsv, alt: "btcsv"},
-          {src: eth, alt: "eth"},
-          {src: eos, alt: "eos"},
-          {src: bnb, alt: "bnb"},
-          {src: bts, alt: "bts"},
-          {src: akash, alt: "akash"},
-          {src: polygon, alt: "polygon"},
-          {src: ar, alt: "ar"},
-          {src: atom, alt: "atom"},
-          {src: zen, alt: "zen"},
-          {src: avax, alt: "avax"}
+          { src: stc, alt: "stc" },
+          { src: btc, alt: "btc" },
+          { src: rvn, alt: "rvn" },
+          { src: btcsv, alt: "btcsv" },
+          { src: eth, alt: "eth" },
+          { src: eos, alt: "eos" },
+          { src: bnb, alt: "bnb" },
+          { src: bts, alt: "bts" },
+          { src: akash, alt: "akash" },
+          { src: polygon, alt: "polygon" },
+          { src: ar, alt: "ar" },
+          { src: atom, alt: "atom" },
+          { src: zen, alt: "zen" },
+          { src: avax, alt: "avax" }
         ],
         [
-          {src: tribe, alt: "tribe"},
-          {src: ckb, alt: "ckb"},
-          {src: zec, alt: "zec"},
-          {src: dot, alt: "dot"},
-          {src: ksm, alt: "ksm"},
-          {src: dash, alt: "dash"},
-          {src: algo, alt: "algo"},
-          {src: dcr, alt: "dcr"},
-          {src: ltc, alt: "ltc"},
-          {src: bsc, alt: "bsc"},
-          {src: mob, alt: "mob"},
-          {src: xmr, alt: "xmr"},
-          {src: near, alt: "near"},
-          {src: alpaca, alt: "alpaca"}
+          { src: tribe, alt: "tribe" },
+          { src: ckb, alt: "ckb" },
+          { src: zec, alt: "zec" },
+          { src: dot, alt: "dot" },
+          { src: ksm, alt: "ksm" },
+          { src: dash, alt: "dash" },
+          { src: algo, alt: "algo" },
+          { src: dcr, alt: "dcr" },
+          { src: ltc, alt: "ltc" },
+          { src: bsc, alt: "bsc" },
+          { src: mob, alt: "mob" },
+          { src: xmr, alt: "xmr" },
+          { src: near, alt: "near" },
+          { src: alpaca, alt: "alpaca" }
         ],
         [
-          {src: ton, alt: "ton"},
-          {src: hmt, alt: "hmt"},
-          {src: nmc, alt: "nmc"},
-          {src: sc, alt: "sc"},
-          {src: sol, alt: "sol"},
-          {src: xtz, alt: "xtz"},
-          {src: trx, alt: "trx"},
-          {src: v, alt: "v"},
-          {src: nem, alt: "nem"},
-          {src: xlm, alt: "xlm"},
-          {src: p, alt: "p"},
-          {src: doge, alt: "doge"},
-          {src: etc, alt: "etc"},
-          {src: xrp, alt: "xrp"}
+          { src: ton, alt: "ton" },
+          { src: hmt, alt: "hmt" },
+          { src: nmc, alt: "nmc" },
+          { src: sc, alt: "sc" },
+          { src: sol, alt: "sol" },
+          { src: xtz, alt: "xtz" },
+          { src: trx, alt: "trx" },
+          { src: v, alt: "v" },
+          { src: nem, alt: "nem" },
+          { src: xlm, alt: "xlm" },
+          { src: p, alt: "p" },
+          { src: doge, alt: "doge" },
+          { src: etc, alt: "etc" },
+          { src: xrp, alt: "xrp" }
         ]
       ]
     }
   },
+  methods: {
+    fillIconList() {
+      let totalRowNumber = 1;
+      let totalWidth = this.rowWidth - (innerWidth < 720 ? this.leftOffsets[0] / 3 : this.leftOffsets[0]);
+
+      if (innerWidth > totalWidth) {
+        while(totalWidth < innerWidth) {
+          totalWidth += this.rowWidth;
+          totalRowNumber += 1;
+        }
+      }
+      
+      this.iconList = this.iconList.map(row => new Array(totalRowNumber + 1).fill(0).reduce(
+        (prev) => [...prev, ...row], 
+        []
+      ));
+    }
+  },
+  mounted() {
+    this.fillIconList();
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.animation-container1 {
+  animation: scroll-animation1 15s linear infinite;
+}
+.animation-container2 {
+  animation: scroll-animation2 15s linear infinite;
+}
+.animation-container3 {
+  animation: scroll-animation3 15s linear infinite;
+}
+
+@media screen and (max-width: 720px) {
+  .animation-container1 {
+    animation: mobile-scroll-animation1 15s linear infinite;
+  }
+  .animation-container2 {
+    animation: mobile-scroll-animation2 15s linear infinite;
+  }
+  .animation-container3 {
+    animation: mobile-scroll-animation3 15s linear infinite;
+  }
+}
+
+@keyframes scroll-animation1 {
+  from {
+    left: -27px;
+  }
+
+  to {
+    left: -1763px;
+  }
+}
+@keyframes scroll-animation2 {
+  from {
+    left: 36px;
+  }
+
+  to {
+    left: -1700px;
+  }
+}
+@keyframes scroll-animation3 {
+  from {
+    left: 12px;
+  }
+
+  to {
+    left: -1724px;
+  }
+}
+@keyframes mobile-scroll-animation1 {
+  from {
+    left: -9px;
+  }
+
+  to {
+    left: -681px;
+  }
+}
+@keyframes mobile-scroll-animation2 {
+  from {
+    left: 12px;
+  }
+
+  to {
+    left: -660px;
+  }
+}
+@keyframes mobile-scroll-animation3 {
+  from {
+    left: 4px;
+  }
+
+  to {
+    left: -668px;
+  }
+}
+</style>
